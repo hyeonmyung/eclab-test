f4da514572436c175075326ac9686868269791f2
hmpark <xeptem4er@gmail.com>
2024-09-03 17:21:51 +0900

    feat: 학생용 화면 생성 및 api 연동

---
+++ src/app/stu/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import CommonLayout from "@/components/CommonLayout";
+import ReportDetailCard from "@/components/ReportDetailCard";
+import Title from "@/components/Title";
+import axios from "axios";
+import { FunctionComponent, useCallback, useEffect, useState } from "react";
+import { StudentDataTypes } from "../types/studentDataTypes";
+
+const StuPage: FunctionComponent = () => {
+  const [studentDataLists, setStudentDataLists] =
+    useState<StudentDataTypes | null>(null);
+
+  const getStudentData = useCallback(async () => {
+    await axios.get(`https://edu.eclab.me/api/test`).then((res) => {
+      if (res.status === 200) {
+        setStudentDataLists(res.data.data);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    getStudentData();
+  }, [getStudentData]);
+  console.log(studentDataLists);
+  return (
+    <CommonLayout page="STU">
+      <Title>EC Report Details</Title>
+      <ReportDetailCard studentDataLists={studentDataLists} />
+    </CommonLayout>
+  );
+};
+export default StuPage;
