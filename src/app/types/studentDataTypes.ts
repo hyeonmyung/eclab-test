0e90beddfae12ef24ccc3f94f6d80e3602998b8e
hmpark <xeptem4er@gmail.com>
2024-09-04 17:33:09 +0900

    feat: 관리자 화면 내 사용되는 데이터 타입 추가

--- src/app/types/studentDataTypes.ts
+++ src/app/types/studentDataTypes.ts
@@ -4,11 +4,37 @@ export interface StudentData {
 export interface StudentDataTypes {
   filter: any;
   title: string;
+  student: {
+    name: string;
+    email: string;
+    profile: {
+      id: string;
+      school_id: string;
+      gender: string;
+      birth_day: string;
+      grade: string;
+      year_admission: string;
+      nationality: string[];
+      status_type: string;
+      created_at: string;
+      updated_at: string;
+      school: {
+        country_id: string;
+        id: string;
+        name: string;
+        address: string;
+        url: string;
+        created_at: string;
+        updated_at: string;
+      };
+    };
+  };
   counselor: {
     email: string;
     name: string;
   };
   send_dt: string;
+  delivered_dt: string;
   ec_report_items: {
     id: string;
     ec_db: {
